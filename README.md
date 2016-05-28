8740192639976938025e7096f0a75990aeac13e7
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2016-05-28 21:14:40 +0200

    get ready for publishing on GitHub
    Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+# Usage
+
+```
+Digital Ocean command line client
+
+Usage:
+  digitalocean-cli.py [ --debug ] [ --token=TOKEN ] create [ --image=ubuntu-16-04-x64 ] [ --region=ams3 ] [ --ssh=~/.ssh/id_rsa.pub ] [ --size=512mb ] [ --num=1 ]
+  digitalocean-cli.py [ --debug ] [ --token=TOKEN ] list
+  digitalocean-cli.py [ --debug ] [ --token=TOKEN ] power_off ( <name>... | <id>... )
+  digitalocean-cli.py [ --debug ] [ --token=TOKEN ] power_on  ( <name>... | <id>... )
+  digitalocean-cli.py [ --debug ] [ --token=TOKEN ] reboot    ( <name>... | <id>... )
+  digitalocean-cli.py [ --debug ] [ --token=TOKEN ] destroy   ( <name>... | <id>... )
+
+Make sure to set env variable TOKEN (or define your own name using --token) with you know what as a value
+```
--- digitalocean-cli.py
+++ digitalocean-cli.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
 """
-Digital ocean automator
+Digital Ocean command line client
 
 Usage:
-  do-toolbox.py [ --debug ] [ --token=TOKEN ] create [ --image=ubuntu-16-04-x64 ] [ --region=ams3 ] [ --ssh=~/.ssh/id_rsa.pub ] [ --size=512mb ] [ --num=1 ]
-  do-toolbox.py [ --debug ] [ --token=TOKEN ] list
-  do-toolbox.py [ --debug ] [ --token=TOKEN ] power_off ( <name>... | <id>... )
-  do-toolbox.py [ --debug ] [ --token=TOKEN ] power_on  ( <name>... | <id>... )
-  do-toolbox.py [ --debug ] [ --token=TOKEN ] reboot    ( <name>... | <id>... )
-  do-toolbox.py [ --debug ] [ --token=TOKEN ] destroy   ( <name>... | <id>... )
+  digitalocean-cli.py [ --debug ] [ --token=TOKEN ] create [ --image=ubuntu-16-04-x64 ] [ --region=ams3 ] [ --ssh=~/.ssh/id_rsa.pub ] [ --size=512mb ] [ --num=1 ]
+  digitalocean-cli.py [ --debug ] [ --token=TOKEN ] list
+  digitalocean-cli.py [ --debug ] [ --token=TOKEN ] power_off ( <name>... | <id>... )
+  digitalocean-cli.py [ --debug ] [ --token=TOKEN ] power_on  ( <name>... | <id>... )
+  digitalocean-cli.py [ --debug ] [ --token=TOKEN ] reboot    ( <name>... | <id>... )
+  digitalocean-cli.py [ --debug ] [ --token=TOKEN ] destroy   ( <name>... | <id>... )
+
+Make sure to set env variable TOKEN (or define your own name using --token) with you know what as a value
 
 """
 from docopt import docopt
